2ca2457de3944a6bca84102e19d9f12ab968e4e9
Joy <biwott.joy.03@gmail.com>
2023-05-28 07:46:24 +0300

    react-router-routes

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorElements=directors.map((director)=> 
+      <div key={director.name}>
+        <h3>{director.name}</h3>
+       
+        <ul>
+          {director.movies.map((movie, index)=>
+            <li key={index}>{movie}</li>
+          )}
+        </ul>
+      </div>)
+    
+  return <div>{/*{code here}*/}
+    <h1>Directors Page</h1>
+    {directorElements}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const movieElements=movies.map((movie)=>
+    <div key={movie.title}>
+      <h3>{movie.title}</h3>
+      <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map((genre, index)=>
+            <li key={index}>{genre}</li>
+          )}
+        </ul>
+    </div>
+  )
+  return <div>{/*{code here}*/}
+    <h1>Movies Page</h1>
+    {movieElements}
+  </div>;
 }
 
 export default Movies;
